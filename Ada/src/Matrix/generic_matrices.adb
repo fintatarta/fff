5589b9dda9fd24efda15b6956bfceb08b23bcf60
Riccardo Bernardini <riccardo.bernardini@uniud.it>
2023-03-30 11:03:11 +0200

    Added draft of generic_matrix package

---
+++ Ada/src/Matrix/generic_matrices.adb
@@ -0,0 +1,304 @@
+pragma Ada_2012;
+package body Generic_Matrices is
+
+   function "-" (X, Y : Ring_Type) return Ring_Type
+   is (X + (-Y));
+
+   ------------
+   -- N_Rows --
+   ------------
+
+   function N_Rows (X : Matrix) return Positive
+   is (X.N_Rows);
+
+   ------------
+   -- N_Cols --
+   ------------
+
+   function N_Cols (X : Matrix) return Positive
+   is (X.N_Cols);
+
+
+   -----------
+   -- Value --
+   -----------
+
+   function Value (X : Matrix; N : Index_Type) return Ring_Type is
+   begin
+      if not X.Is_Vector or N > X.Length then
+         raise Constraint_Error;
+      end if;
+
+      return X.Data (N);
+   end Value;
+
+   -----------
+   -- Value --
+   -----------
+
+   function Value (X : Matrix; Row, Col : Index_Type) return Ring_Type is
+   begin
+      if Row > X.N_Rows or Col > X.N_Cols then
+         raise Constraint_Error;
+      end if;
+
+      return X.Data (To_Index (X, Row, Col));
+   end Value;
+
+   ---------------
+   -- Reference --
+   ---------------
+
+   function Reference
+     (X : aliased in out Matrix; N : in Index_Type) return Reference_Type
+   is
+   begin
+      if not X.Is_Vector or N > X.Length then
+         raise Constraint_Error;
+      end if;
+
+      return Reference_Type'
+        (Element => Ring_Vectors.Reference (X.Data, N).Element);
+   end Reference;
+
+   ---------------
+   -- Reference --
+   ---------------
+
+   function Reference
+     (X : aliased in out Matrix; Row, Col : in Index_Type)
+      return Reference_Type
+   is
+      use Ring_Vectors;
+   begin
+      if Row > X.N_Rows or Col > X.N_Cols then
+         raise Constraint_Error;
+      end if;
+      return Reference_Type'
+        (Element => Reference (X.Data, To_Index (X, Row, Col)).Element);
+   end Reference;
+
+   ----------
+   -- Zero --
+   ----------
+
+   function Zero (N : Index_Type) return Matrix
+   is (Zero (N, N));
+
+   ----------
+   -- Zero --
+   ----------
+
+   function Zero (X : Matrix) return Matrix
+   is (Zero (N_Rows => X.N_Rows,
+             N_Cols => X.N_Cols));
+
+   ----------
+   -- Zero --
+   ----------
+
+   function Zero (N_Rows, N_Cols : Index_Type) return Matrix
+   is
+      use Ring_Vectors;
+   begin
+      return Matrix'(N_Rows => N_Rows,
+                     N_Cols => N_Cols,
+                     Data   =>
+                       To_Vector (New_Item => Ring_Zero,
+                                  Length   => Count_Type (N_Rows * N_Cols)));
+   end Zero;
+
+   --------------
+   -- Identity --
+   --------------
+
+   function Identity (N : Index_Type) return Matrix
+   is (Identity (N, N));
+
+   --------------
+   -- Identity --
+   --------------
+
+   function Identity (X : Matrix) return Matrix
+   is (Identity (N_Rows => X.N_Rows,
+                 N_Cols => X.N_Cols));
+
+   --------------
+   -- Identity --
+   --------------
+
+   function Identity (N_Rows, N_Cols : Index_Type) return Matrix is
+   begin
+      return Result : Matrix := Zero (N_Rows => N_Rows, N_Cols => N_Cols) do
+         for I in Index_Type'First .. Index_Type'Min (N_Rows, N_Cols) loop
+            Result (I, I) := Ring_One;
+         end loop;
+      end return;
+   end Identity;
+
+   ---------
+   -- "+" --
+   ---------
+
+   function "+" (X, Y : Matrix) return Matrix is
+   begin
+      if not (X.N_Rows = Y.N_Rows and X.N_Cols = Y.N_Cols) then
+         raise Constraint_Error;
+      end if;
+
+      pragma Assert (X.Data.First_Index = Y.Data.First_Index);
+      pragma Assert (X.Data.Last_Index = Y.Data.Last_Index);
+
+      return Result : Matrix := Zero (X) do
+         pragma Assert (X.Data.First_Index = Result.Data.First_Index);
+         pragma Assert (X.Data.Last_Index = Result.Data.Last_Index);
+
+         for I in X.Data.First_Index .. X.Data.Last_Index loop
+            Result.Data (I) := X.Data (I) + Y.Data (I);
+         end loop;
+      end return;
+   end "+";
+
+   ---------
+   -- "-" --
+   ---------
+
+   function "-" (X, Y : Matrix) return Matrix is
+   begin
+      if not (X.N_Rows = Y.N_Rows and X.N_Cols = Y.N_Cols) then
+         raise Constraint_Error;
+      end if;
+
+      pragma Assert (X.Data.First_Index = Y.Data.First_Index);
+      pragma Assert (X.Data.Last_Index = Y.Data.Last_Index);
+
+      return Result : Matrix := Zero (X) do
+         pragma Assert (X.Data.First_Index = Result.Data.First_Index);
+         pragma Assert (X.Data.Last_Index = Result.Data.Last_Index);
+
+         for I in X.Data.First_Index .. X.Data.Last_Index loop
+            Result.Data (I) := X.Data (I) - Y.Data (I);
+         end loop;
+      end return;
+   end "-";
+
+   ---------
+   -- "-" --
+   ---------
+
+   function "-" (X : Matrix) return Matrix is
+   begin
+      return Result : Matrix := Zero (X) do
+         pragma Assert (X.Data.First_Index = Result.Data.First_Index);
+         pragma Assert (X.Data.Last_Index = Result.Data.Last_Index);
+
+         for I in X.Data.First_Index .. X.Data.Last_Index loop
+            Result.Data (I) := - X.Data (I);
+         end loop;
+      end return;
+   end "-";
+
+   ---------
+   -- Row --
+   ---------
+
+   function Row (X : Matrix; R : Index_Type) return Matrix
+   is
+   begin
+      return Result : Matrix := Zero (N_Rows => 1,
+                                      N_Cols => X.N_Cols) do
+         for I in 1 .. X.N_Cols loop
+            Result.Data (I) := X (R, I);
+         end loop;
+      end return;
+   end Row;
+
+   ------------
+   -- Column --
+   ------------
+
+   function Column (X : Matrix; C : Index_Type) return Matrix
+   is
+   begin
+      return Result : Matrix := Zero (N_Rows => X.N_Rows,
+                                      N_Cols => 1) do
+         for I in 1 .. X.N_Rows loop
+            Result.Data (I) := X (I, C);
+         end loop;
+      end return;
+   end Column;
+
+
+   --------------------
+   -- Scalar_Product --
+   --------------------
+
+   function Scalar_Product (X, Y : Matrix) return Ring_Type
+   is
+
+   begin
+      if not (X.Is_Vector and Y.Is_Vector and X.Length = Y.Length) then
+         raise Constraint_Error;
+      end if;
+
+      return  Accumulator : Ring_Type := Ring_Zero do
+         for I in 1 .. X.Length loop
+            Accumulator := Accumulator + X.Data (I) * Y.Data (I);
+         end loop;
+      end return;
+   end Scalar_Product;
+
+
+   ---------
+   -- "*" --
+   ---------
+
+   function "*" (X, Y : Matrix) return Matrix is
+
+   begin
+      if X.N_Cols /= Y.N_Rows then
+         raise Constraint_Error;
+      end if;
+
+      return Result : Matrix := Zero (N_Rows => X.N_Rows,
+                                      N_Cols => Y.N_Cols) do
+
+         for R in 1 .. Result.N_Rows loop
+            for C in 1 .. Result.N_Cols loop
+               Result (R, C) := Scalar_Product (Row (X, R), Column (Y, C));
+            end loop;
+         end loop;
+
+      end return;
+   end "*";
+
+   ---------
+   -- "*" --
+   ---------
+
+   function "*" (C : Ring_Type; X : Matrix) return Matrix is
+   begin
+      return Result : Matrix := Zero (X) do
+         pragma Assert (X.Data.First_Index = Result.Data.First_Index);
+         pragma Assert (X.Data.Last_Index = Result.Data.Last_Index);
+
+         for I in X.Data.First_Index .. X.Data.Last_Index loop
+            Result.Data (I) := C * X.Data (I);
+         end loop;
+      end return;
+   end "*";
+
+   ----------
+   -- "**" --
+   ----------
+
+   function "**" (X : Matrix; Exponent : Natural) return Matrix is
+   begin
+      return Result : Matrix := Identity (X) do
+         for I in 1 .. Exponent loop
+            Result := Result * X;
+         end loop;
+      end return;
+   end "**";
+
+end Generic_Matrices;
---
+++ Ada/src/Matrix/generic_matrices.ads
@@ -0,0 +1,190 @@
+with Ada.Containers.Vectors;
+
+generic
+   type Ring_Type is private;
+
+   Ring_Zero : Ring_Type;
+   Ring_One  : Ring_Type;
+
+   with function "+" (X, Y : Ring_Type) return Ring_Type is <>;
+   with function "-" (X : Ring_Type) return Ring_Type is <>;
+   with function "*" (X, Y : Ring_Type) return Ring_Type is <>;
+
+   with function Is_Unit (X : Ring_Type) return Boolean is <>;
+   with function Inv (X : Ring_Type) return Ring_Type is <>;
+package Generic_Matrices is
+   type Matrix is tagged private
+     with
+       Constant_Indexing => Value,
+       Variable_Indexing => Reference;
+
+   subtype Index_Type is Positive;
+
+   function N_Rows (X : Matrix) return Positive;
+   function N_Cols (X : Matrix) return Positive;
+
+   subtype Column_Vector is Matrix
+     with
+       Dynamic_Predicate => Column_Vector.N_Cols = 1;
+
+   subtype Row_Vector is Matrix
+     with
+       Dynamic_Predicate => Row_Vector.N_Rows = 1;
+
+   subtype Square_Matrix is Matrix
+     with
+       Dynamic_Predicate => Square_Matrix.N_Rows = Square_Matrix.N_Cols;
+
+   function Is_Vector (X : Matrix) return Boolean
+   is (X.N_Rows = 1 or X.N_Cols = 1);
+
+   function Length (X : Matrix) return Positive
+   is (Positive'Max (X.N_Rows, X.N_Cols));
+
+   function Value (X : Matrix; N : Index_Type) return Ring_Type
+     with
+       Pre => X.Is_Vector and X.Length >= N;
+
+   function Value (X : Matrix; Row, Col : Index_Type) return Ring_Type
+     with
+       Pre => Row <= X.N_Rows and Col <= X.N_Cols;
+
+   type Reference_Type (Element : not null access Ring_Type) is private
+     with Implicit_Dereference => Element;
+
+   function Reference (X : aliased in out Matrix;
+                       N : in Index_Type)
+                       return Reference_Type
+     with
+       Pre => X.Is_Vector and N <= X.Length;
+
+   function Reference (X        : aliased in out Matrix;
+                       Row, Col : in Index_Type)
+                       return Reference_Type
+     with
+       Pre => Row <= X.N_Rows and Col <= X.N_Cols;
+
+   function Zero (N : Index_Type) return Matrix
+     with
+       Post =>
+         Zero'Result.N_Rows = N and
+         Zero'Result.N_Cols = N;
+
+   function Zero (N_Rows, N_Cols : Index_Type) return Matrix
+     with
+       Post =>
+         Zero'Result.N_Rows = N_Rows and
+         Zero'Result.N_Cols = N_Cols;
+
+   function Zero (X : Matrix) return Matrix
+     with
+       Post =>
+         Zero'Result.N_Rows = X.N_Rows and
+         Zero'Result.N_Cols = X.N_Cols;
+
+   function Identity (N : Index_Type) return Matrix
+     with
+       Post =>
+         Identity'Result.N_Rows = N and
+         Identity'Result.N_Cols = N;
+
+   function Identity (X : Matrix) return Matrix
+     with
+       Post =>
+         Identity'Result.N_Rows = X.N_Rows and
+         Identity'Result.N_Cols = X.N_Cols;
+
+
+   function Identity (N_Rows, N_Cols : Index_Type) return Matrix
+     with
+       Post =>
+         Identity'Result.N_Rows = N_Rows and
+         Identity'Result.N_Cols = N_Cols;
+
+   function "+" (X, Y : Matrix) return Matrix
+     with
+       Pre =>
+         X.N_Rows = Y.N_Rows and
+         X.N_Cols = Y.N_Cols,
+         Post =>
+           X.N_Rows = "+"'Result.N_Rows and
+           X.N_Cols = "+"'Result.N_Cols;
+
+   function "-" (X, Y : Matrix) return Matrix
+     with
+       Pre =>
+         X.N_Rows = Y.N_Rows and
+         X.N_Cols = Y.N_Cols,
+         Post =>
+           X.N_Rows = "-"'Result.N_Rows and
+           X.N_Cols = "-"'Result.N_Cols;
+
+   function "-" (X : Matrix) return Matrix
+     with
+       Post =>
+         X.N_Rows = "-"'Result.N_Rows and
+         X.N_Cols = "-"'Result.N_Cols;
+
+   function "*" (X, Y : Matrix) return Matrix
+     with
+       Pre => X.N_Cols = Y.N_Rows,
+       Post =>
+         X.N_Rows = "*"'Result.N_Rows and
+         Y.N_Cols = "*"'Result.N_Cols;
+
+   function "*" (C : Ring_Type; X : Matrix) return Matrix
+     with
+       Post =>
+         X.N_Rows = "*"'Result.N_Rows and
+         X.N_Cols = "*"'Result.N_Cols;
+
+   function "**" (X : Matrix; Exponent : Natural) return Matrix
+     with
+       Pre =>
+         X.N_Rows = X.N_Cols,
+         Post =>
+           X.N_Rows = "**"'Result.N_Rows and
+           X.N_Cols = "**"'Result.N_Cols;
+
+   function Row (X : Matrix; R : Index_Type) return Matrix
+     with
+       Pre => R <= X.N_Rows,
+       Post => Row'Result.N_Rows = 1 and Row'Result.N_Cols = X.N_Cols;
+
+   function Column (X : Matrix; C : Index_Type) return Matrix
+     with
+       Pre => C <= X.N_Cols,
+       Post => Column'Result.N_Cols = 1 and Column'Result.N_Rows = X.N_Rows;
+
+   function Scalar_Product (X, Y : Matrix) return Ring_Type
+     with
+       Pre => X.Is_Vector and Y.Is_Vector and X.Length = Y.Length;
+
+private
+   use Ada.Containers;
+
+   --  function Create (N_Row, N_Cols : Positive) return Matrix
+   --    with
+   --      Post =>
+   --        Create'Result.N_Rows = N_Rows and
+   --        Create'Result.N_Cols = N_Cols;
+
+   package Ring_Vectors is
+     new Ada.Containers.Vectors (Index_Type   => Index_Type,
+                                 Element_Type => Ring_Type);
+
+   type Matrix is tagged
+      record
+         N_Rows : Positive;
+         N_Cols : Positive;
+         Data   : Ring_Vectors.Vector;
+      end record
+     with
+       Type_Invariant => Data.Length = Count_Type (N_Rows * N_Cols);
+
+   function To_Index (M : Matrix; Row, Col : Index_Type) return Index_Type
+   is (Row + (Col - Index_Type'First) * M.N_Rows);
+
+   type Reference_Type (Element : not null access Ring_Type) is null record;
+
+end Generic_Matrices;
